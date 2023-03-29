80bc85ab1adaede6acab129c75c7a00c799ee505
IHEEB22 <iheb.hmida1000@gmail.com>
2023-03-29 01:40:21 +0100

    add package-lock.json file

--- app/server.js
+++ app/server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}!`);
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
 });
